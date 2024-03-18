d625ec7e1ee64a42d9e167d3998cdaa379d7071a
oliebueno <124280001+oliebueno@users.noreply.github.com>
2024-03-18 16:51:13 -0400

    lectura de archivos json

---
+++ read_json.py
@@ -0,0 +1,95 @@
+import json
+from datetime import datetime, timedelta
+
+# Función para leer un archivo JSON
+
+
+def read_json_file(file):
+    try:
+        with open(file, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f'El archivo {file} no existe')
+        return None
+
+
+# Función para obtener el rango de las fechas de un archivo JSON
+
+
+def get_dates(data):
+    start_date = datetime.fromisoformat(data['start_date'])
+    end_date = datetime.fromisoformat(data['end_date'])
+
+    # Se genera el rango de fechas
+    range_dates = []
+    actual_date = start_date
+    while actual_date <= end_date:
+        range_dates.append(actual_date.strftime('%Y-%m-%d'))
+        actual_date += timedelta(days=1)
+
+    return range_dates
+
+# Función para obtener el rango de las horas de un archivo JSON
+
+
+def get_hours(data):
+    start_hour = datetime.strptime(data['start_time'], '%H:%M:%S')
+    end_hour = datetime.strptime(data['end_time'], '%H:%M:%S').time()
+
+    # Se redondea la hora de inicio a la siguiente hora en punto
+    if start_hour.minute != 0 or start_hour.second != 0:
+        start_hour = start_hour.replace(
+            minute=0, second=0) + timedelta(hours=1)
+
+    # Se genera el rango de horas
+    range_hours = []
+    actual_hour = start_hour
+    while actual_hour.time() <= end_hour:
+        range_hours.append(actual_hour.strftime('%H:%M:%S'))
+        actual_hour += timedelta(hours=2)
+
+        # Verificar si la hora actual más dos horas excede la hora de fin
+        if actual_hour.time() < datetime.strptime('01:59:59', '%H:%M:%S').time():
+            break
+
+    return range_hours
+
+
+# Función para obtener los equipos de un archivo JSON
+
+
+def get_teams(data):
+    return data['participants']
+
+
+# Función para obtener el nombre del torneo de un archivo JSON
+
+
+def get_inicial_date(data):
+    return datetime.fromisoformat(data['start_date']).date()
+
+
+# Función para obtener el nombre del torneo de un archivo JSON
+
+
+def get_final_date(data):
+    return datetime.fromisoformat(data['end_date']).date()
+
+# Función para obtener el nombre del torneo de un archivo JSON
+
+
+def get_inicial_time(data):
+    return datetime.strptime(data['start_time'], '%H:%M:%S').time()
+
+# Función para obtener el nombre del torneo de un archivo JSON
+
+
+def get_final_time(data):
+    return datetime.strptime(data['end_time'], '%H:%M:%S').time()
+
+# Función para obtener el nombre del torneo de un archivo JSON
+
+
+def get_tournament_name(data):
+    return data['tournament_name']
