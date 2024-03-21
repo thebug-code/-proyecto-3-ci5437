14590e79caca14ecb0c0ed7809a7c2e563abf842
oliebueno <124280001+oliebueno@users.noreply.github.com>
2024-03-21 12:59:18 -0400

    Glucose

---
+++ glucose-4.2.1/simp/depend.mk
@@ -0,0 +1,40 @@
+/home/oliver/proyecto3/glucose-4.2.1/simp/Main.o /home/oliver/proyecto3/glucose-4.2.1/simp/Main.or /home/oliver/proyecto3/glucose-4.2.1/simp/Main.od /home/oliver/proyecto3/glucose-4.2.1/simp/Main.op: /home/oliver/proyecto3/glucose-4.2.1/simp/Main.cc \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../utils/System.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/IntTypes.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../utils/ParseUtils.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../utils/Options.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Vec.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/XAlloc.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/Dimacs.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/SolverTypes.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Alg.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Map.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Alloc.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../simp/SimpSolver.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Queue.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/Solver.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Heap.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/BoundedQueue.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/Constants.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Clone.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/SolverStats.h
+/home/oliver/proyecto3/glucose-4.2.1/simp/SimpSolver.o /home/oliver/proyecto3/glucose-4.2.1/simp/SimpSolver.or /home/oliver/proyecto3/glucose-4.2.1/simp/SimpSolver.od /home/oliver/proyecto3/glucose-4.2.1/simp/SimpSolver.op: /home/oliver/proyecto3/glucose-4.2.1/simp/SimpSolver.cc \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Sort.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Vec.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/IntTypes.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/XAlloc.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../simp/SimpSolver.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Queue.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/Solver.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Heap.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Alg.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../utils/Options.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../utils/ParseUtils.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/SolverTypes.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Map.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Alloc.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/BoundedQueue.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/Constants.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../mtl/Clone.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../core/SolverStats.h \
+ /home/oliver/proyecto3/glucose-4.2.1/simp/../utils/System.h
