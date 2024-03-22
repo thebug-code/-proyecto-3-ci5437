58ab2de47007f90f74bd35e62ab41cc14d8945ce
admeneses <alejandrocardie12@gmail.com>
2024-03-21 23:50:13 -0400

    update utils

--- utils.py
+++ utils.py
@@ -1,17 +1,38 @@
 import subprocess
 import sys
 
-def run_glucose(cnf_file_path):
+def get_keys_by_value(dictionary, value_to_find):
+    return [key for key, value in dictionary.items() if value == value_to_find]
+
+def run_glucose(cnf_file_path, traductor):
     # Comando para ejecutar glucose con el archivo CNF en formato Dimacs
-    command = ['glucose/simp/glucose', cnf_file_path, "-model", "-verb=0"]
+    command = ['glucose/simp/glucose', cnf_file_path, "glucose-sol.txt", "-model", "-verb=0"]
     
-    # Abrir un archivo de texto para escribir la salida
-    with open('glucose-sol.txt', 'w') as out_file:
-        # Ejecutar Glucose y redirigir la salida al archivo
-        subprocess.run(command, stdout=out_file, stderr=subprocess.PIPE, text=True)
+    # Ejecutar Glucose
+    subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     
-if __name__ == '__main__':
-    run_glucose(sys.argv[1])
+    with open("glucose-sol.txt", 'r') as file:
+        solution = file.readline().strip()
+        file.close()
 
-
-
+    # Verifica si la solucion existe
+    if solution == "UNSAT":
+        print("Fórmula lógica insatisfacible")
+        exit()
+    else:
+        traductor.ical(solution)
+        print(f"{cnf_file_path}.isc fue creado exitosamente")
+    
+        # Obtiene el numero de clausulas y variables creadas
+        with open(cnf_file_path, 'r') as file:
+            first_line = file.readline().strip()
+            file.close()
+    
+        heads = first_line.split()
+        variables = int(heads[2])
+        clauses = int(heads[3])
+    
+        print()
+        print("Numero de variables y clausulas en DIMACS CNF:")
+        print(f"- {variables} variables")
+        print(f"- {clauses} clausulas")
