050fa019b19d3353fb1401a4fcc3b4e18b3e24d9
admeneses <alejandrocardie12@gmail.com>
2024-03-21 23:51:00 -0400

    al main todavia le falta

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from traductor_cnf import TraductorCNF
 import read_json as rj
+import utils as util
 
 # prueba
 file = 'ejemplo1.json'
 data = rj.read_json_file(file)
 traductor = TraductorCNF(data)
 matches = traductor.match_generator()
-print(matches)
-cnf = traductor.one_team_one_opponent(matches, [])
-nombre = rj.get_tournament_name(data)
-print(nombre)
-traductor.dismacs(len(matches), cnf, nombre)
+clauses = traductor.cnf_clause()
+tournament_name = rj.get_tournament_name(data)
+traductor.dismacs(len(matches), clauses, tournament_name)
+cnf_file_path = file.split('.')[0] + '.dimacs'
+util.run_glucose(cnf_file_path, traductor)
+
